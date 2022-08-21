6f7effd109776807bb74ddb74611c3829a717f05
Joe Rodini <joseph.rodini@gmail.com>
2022-08-21 15:51:36 -0700

    11.5.1.

--- static/js/app.js
+++ static/js/app.js
@@ -3,3 +3,9 @@ const tableData = data;
 
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
+
+//create build table function
+
+function buildTable(data) {
+    tbody.html("");
+  }
